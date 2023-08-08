4528563544e85e143e460376cd749dbaae3ec366
viral mandaviya <140678543+Veer2127@users.noreply.github.com>
2023-08-08 21:02:25 +0530

    Module – 2 (Fundamentals of python).py

--- Module – 2 (Fundamentals of python).py
+++ Module – 2 (Fundamentals of python).py
@@ -237,6 +237,9 @@ else:
 
 #20)Write a Python function that takes a list of words and returns the length of the longest one.
 
+    mylist=["welcom","viral","tops","python"]
+
+        print(max(mylist))
 
 
 
